5b3bd75ab559115a6eeb3c2c1d9475accc783911
Ignacio <ignacio.delavega@ing.austral.edu.ar>
2017-10-06 13:19:20 -0300

    ola

---
+++ src/tpEspecial/Reader.java
@@ -0,0 +1,66 @@
+package tpEspecial;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Reader {
+
+    public List<String> readFile(String filename, HashMap<String, Integer> hashMap){
+        List<String> list = new ArrayList<>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+            String line = br.readLine();
+
+            while(line!=null){
+                if(!line.equals("")) {
+                    list.add(line);
+                    hashMap.put(line, 0);
+                }
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public void readHTML(String filename, HashMap<String, Integer> words, State automata){
+        State currentState = automata;
+        try {
+            FileReader fileReader = new FileReader(filename);
+            int read = fileReader.read();
+            String word = "";
+            boolean ignoring = false;
+            while (read != -1){
+                if(word.equals("<"))ignoring = true;
+                if(word.equals(">"))ignoring = false;
+                if(ignoring){
+                    read = fileReader.read();
+                    continue;
+                }
+
+                char character = (char) read;
+                word += character;
+                currentState = countChar(currentState, character);
+                if(currentState.isAcceptance()){
+                    Integer value = words.put(word, 1);
+                    words.put(word, value + 1);
+                }
+                read = fileReader.read();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private State countChar(State automata, char character){
+        State state = automata.transition(character);
+        if(state == null) throw new IndexOutOfBoundsException();
+        return state;
+    }
+}
