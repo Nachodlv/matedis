27aebd76446e227ffed52f2b3dda3cf676d3e17e
Giansco <gianniscolar@gmail.com>
2017-10-06 13:01:17 -0300

    fixed construct automata

--- src/tpEspecial/CreateAutomata.java
+++ src/tpEspecial/CreateAutomata.java
@@ -6,21 +6,20 @@ import java.util.List;
  * Created by Gianni on 10/6/2017.
  */
 public class CreateAutomata {
-    State head;
-    List<String> phrases;
 
-    public CreateAutomata(List<String> phrases) {
-        this.phrases = phrases;
+    public static State createAutomata(List<String> phrases) {
+        StateNDA head = new StateNDA(false);
         for (int i = 0; i < phrases.size(); i++) {
-            addPhrase(phrases.get(i));
+            addPhrase(phrases.get(i),head);
         }
+        return head;
     }
 
-    public void addPhrase(String phrase){
+    public static void addPhrase(String phrase, State head){
         State state = head;
         for (int i = 0; i < phrase.length(); i++) {
             char character = phrase.charAt(i);
-            final State transition = state.transitions(character);
+            final State transition = new StateImpl(i==phrase.length()-1);
             state.addTransition(transition, character);
             state=transition;
         }
