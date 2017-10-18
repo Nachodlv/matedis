5e3f916d2e3390045d48db16d4b84b901bd0fb88
Dwape <eduardo.lalor@ing.austral.edu.ar>
2017-10-18 19:26:52 -0300

    Grapher should now work correctly both with NDA and DA.

--- src/tpEspecial/Grapher.java
+++ src/tpEspecial/Grapher.java
@@ -11,7 +11,7 @@ import java.util.Map;
 public class Grapher {
     //int label = 0;
 
-    public void graphNDA(State automata){
+    public static void graphNDA(State automata){
         State currentAutomata = automata;
         try {
             BufferedWriter bw= new BufferedWriter(new FileWriter("src/tpEspecial/grafoNDA.txt"));
@@ -35,14 +35,14 @@ public class Grapher {
         }
     }
 
-    private String connectNodes(State stateA, State stateB, char connect, String sentence){
+    private static String connectNodes(State stateA, State stateB, char connect, String sentence){
         for(Map.Entry<Character,State> entry: ((StateImpl)stateB).getTransitions().entrySet()){
             return connectNodes(stateB,entry.getValue(), entry.getKey(), sentence.concat(transition(stateA, stateB, connect)));
         }
         return sentence.concat(transition(stateA, stateB, connect));
     }
 
-    private String writeAllNodes(State state, String sentence){
+    private static String writeAllNodes(State state, String sentence){
         HashMap<Character, State> hashMap = ((StateImpl) state).getTransitions();
         for(State nextState: hashMap.values()){
             return writeAllNodes(nextState, sentence.concat(writeNode(nextState)));
@@ -50,7 +50,7 @@ public class Grapher {
          return sentence;
     }
 
-    private String writeNode(State automata){
+    private static String writeNode(State automata){
         //automata.setLabel(String.valueOf(label));
         //label++;
         if(automata.isAcceptance()){
@@ -60,17 +60,21 @@ public class Grapher {
         }
     }
 
-    private String transition(State a, State b, Character character){
+    private static String transition(State a, State b, Character character){
         return a.getLabel() + " -> " + b.getLabel() + " [label=\"" + character + "\"];\n";
     }
 
-    public void graphDA(State automata){
+    public static void graphDA(State automata){
         try {
             BufferedWriter bw = new BufferedWriter(new FileWriter("src/tpEspecial/grafoDA.txt"));
             bw.write("digraph{\nrankdir = \"LR\";\n");
 
-            bw.write(writeAllNodesForDA(automata, "", new ArrayList<>()));
-            bw.write(connectNodesForDA((StateImpl) automata, "", automata, new ArrayList<>()));
+            List<String> transitions = new ArrayList<>();
+            writeAllNodesForDA(automata, bw, new ArrayList<>(), transitions, automata);
+            addAllTransitions(transitions, bw);
+            //bw.write(nodes);
+            //bw.write(connectNodesForDA((StateImpl) automata, "", automata, new ArrayList<>()));
+
 
             bw.write("}");
             bw.close();
@@ -79,26 +83,27 @@ public class Grapher {
         }
     }
 
-    private String connectNodesForDA(StateImpl state, String sentence, State head, List<State> visited){
-        sentence= sentence.concat(state.getLabel() + " -> " + head.getLabel() + " [label=\" otros \"];\n");
-        visited.add(state);
-        for(Map.Entry<Character, State> entry: state.getTransitions().entrySet()){
-            if(!visited.contains(entry.getValue())){
-                return connectNodesForDA((StateImpl) entry.getValue(), sentence.concat(transition(state, entry.getValue(), entry.getKey())), head, visited);
-            }
-        }
-        return sentence;
-    }
-
-    private String writeAllNodesForDA(State state, String sentence, List<State> visited){
-        if(visited.contains(state)) return sentence;
+    private static void writeAllNodesForDA(State state, BufferedWriter writer, List<State> visited, List<String> transitions, State head) throws IOException{
+        if(visited.contains(state)) return;
         HashMap<Character, State> hashMap = ((StateImpl) state).getTransitions();
         visited.add(state);
+        addAllTransitions(state, hashMap, transitions, head);
+        writer.write(writeNode(state));
         for(State nextState: hashMap.values()){
-            if(!visited.contains(nextState)) {
-                return writeAllNodesForDA(nextState, sentence.concat(writeNode(nextState)), visited);
-            }
+            writeAllNodesForDA(nextState, writer, visited, transitions, head);
         }
-        return sentence;
+    }
+
+    private static void addAllTransitions(State state, HashMap<Character, State> hashMap, List<String> transitions, State head){
+        transitions.add(state.getLabel() + " -> " + head.getLabel() + " [label=\" otros \"];\n");
+        for(Map.Entry<Character, State> entry: hashMap.entrySet()){
+            transitions.add(transition(state, entry.getValue(), entry.getKey()));
+        }
+    }
+
+    private static void addAllTransitions(List<String> transitions, BufferedWriter writer) throws IOException{
+        for(String transition : transitions){
+            writer.write(transition);
+        }
     }
 }
